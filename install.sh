21bb0e01c27101dd9b9500b18f3cccec917ca4e5
наме-ru <132847268+HAME-RU@users.noreply.github.com>
2023-05-15 16:26:40 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+NC='\033[0m'
+RED='\033[1;38;5;196m'
+GREEN='\033[1;38;5;040m'
+ORANGE='\033[1;38;5;202m'
+BLUE='\033[1;38;5;012m'
+BLUE2='\033[1;38;5;032m'
+PINK='\033[1;38;5;013m'
+GRAY='\033[1;38;5;004m'
+NEW='\033[1;38;5;154m'
+YELLOW='\033[1;38;5;214m'
+CG='\033[1;38;5;087m'
+CP='\033[1;38;5;221m'
+CPO='\033[1;38;5;205m'
+CN='\033[1;38;5;247m'
+CNC='\033[1;38;5;051m'
+echo -e ${RED}    "###############################################################"
+echo -e ${ORANGE} " #     DOMAIN'S TO IP RESOLVERS & NMAP NSE SCRIPT SCAN        #  "
+echo -e ${PINK}   " #             Coded By: HAME-RU                              #  "
+echo -e ${CP}     " #             https://github.com/HAME-RU                     #  "
+echo -e ${RED}    "################################################################ \n "
+d=$(date +"%b-%d-%y %H:%M")
+sleep 1
+echo -e ${CP}"[+]Installtion Started On: $d \n"
+sleep 1
+echo -e ${BLUE}"[+]Checking Go Installation\n"
+
+if [[ -z "$GOPATH" ]]; then
+  echo -e ${RED}"[+]Go is not Installed....Plz Install it and run the script again"
+  echo -e ${CP}"[+]For Installation Plz Check my recon-automation repo pre-requisite part!"
+  exit 1
+  else
+  echo -e ${BLUE}"..........Go is installed..............\n"
+ fi
+echo -e ${GREEN}"[+]Installing Assetfinder\n"
+sleep 1
+
+assetfinder_checking(){
+command -v "assetfinder" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then 
+        go get -u github.com/tomnomnom/assetfinder >/dev/null 2>&1
+        echo -e ".............assetfinder successfully installed..............\n"
+        else
+        echo -e ".......assetfinder already installed..........\n"
+    fi
+
+}
+assetfinder_checking
+sleep 1
+echo -e ${RED}"[+]Installing Seclists\n"
+command -v "seclists" >/dev/null 2>&1
+if [[ ! -d /usr/share/seclists ]]; then 
+        
+        sudo apt install seclists -y
+        echo -e "....................Seclists Successfully Installed.................\n"
+        
+        else
+        echo -e ".................Seclists Already Exists.................\n"
+fi
+      
+sleep 1
+echo -e ${PINK}"[+]Installing Amass\n"
+amass_checking(){
+
+command -v "amass" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         
+         sudo apt-get install amass -y
+         echo -e "................Amass successfully installed..............\n"
+         else
+         echo -e "..........Amass is already installed..........\n"
+   fi
+}
+amass_checking
+sleep 1
+echo -e ${GRAY}"[+]Installing jq\n"
+jq_checking(){
+
+command -v "jq" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         
+         sudo apt-get install jq -y
+         echo -e ".................jq successfully installed..............\n"
+         else
+         echo -e "...........jq is already installed..............\n"
+   fi
+
+}
+jq_checking
+
+sleep 1
+echo -e ${ORANGE}"[+]Installing Subfinder\n"
+subfinder_checking(){
+command -v "subfinder" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         
+         GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder >/dev/null 2>&1
+         echo -e "................subfinder successfully installed..............\n"
+         else
+         echo -e "...........subfinder is already installed.............\n"
+   fi
+
+}
+subfinder_checking
+sleep 1
+echo -e ${YELLOW}"[+]Installing massdns\n"
+massdns_checking(){
+mkdir -p ~/tools
+command -v "massdns" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         cd ~/tools
+         git clone https://github.com/blechschmidt/massdns.git
+         cd massdns
+          make
+          cd bin
+          sudo mv massdns /usr/local/bin
+          echo -e "............massdns installed successfully............\n"
+         else
+         echo -e "..........massdns is already installed............\n"
+    fi
+
+}
+massdns_checking
+sleep 1
+echo -e ${CNC}"[+]Installing dnsvalidator\n"
+dnsvalidator_installing(){
+mkdir -p ~/tools
+mkdir -p ~/tools/resolvers
+
+
+command -v "dnsvalidator" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+        cd ~/tools
+       git clone  https://github.com/vortexau/dnsvalidator.git
+       cd dnsvalidator
+       sudo apt-get install python3-pip -y
+       sudo python3 setup.py install 
+       dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 25 -o resolvers.txt
+       cat resolvers.txt | tail -n 60 > ~/tools/resolvers/resolver.txt
+       else
+        echo -e ".......dnsvalidator already exist.........\n"
+fi
+
+}
+dnsvalidator_installing
+sleep 1
+
+other_tools(){
+echo -e ${CPO}"[+]Installing httpx\n"
+command -v "httpx" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         
+        go get -v github.com/projectdiscovery/httpx/cmd/httpx >/dev/null 2>&1
+        echo -e ".................httpx successfully installed..............\n"
+         else
+         echo -e "...............httpx is already installed.............\n"
+   fi
+
+sleep 1
+echo -e ${CP}"[+]Installing httprobe\n"
+command -v "httprobe" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+         
+         go get -u github.com/tomnomnom/httprobe >/dev/null 2>&1
+         echo -e".............httprobe successfully installed..............\n"
+         else
+         echo -e "...........httprobe is already installed...............\n"
+   fi
+}
+other_tools
+sleep 1
+echo -e ${CG}"[+]Installing NMAP NSE scripts\n"
+nmap_script(){
+     if [ -f /usr/share/nmap/scripts/vulners.nse ]; then
+           
+         echo -e "...............Script already exists................\n"
+         else
+         cd /usr/share/nmap/scripts
+         sudo wget https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
+         echo -e "...............Scripts successfully installed..................\n"
+         
+    fi
+sleep 1
+echo -e ${CP}"[+] Installing vulnscan For NMAP\n"
+    if [ -d /usr/share/nmap/scripts/vulscan ]; then
+               echo -e "....................vulnscan already exists....................\n"
+        else
+        
+        cd /usr/share/nmap/scripts
+        sudo git clone https://github.com/scipag/vulscan.git
+        echo -e "......................vulnscan successfully installed................\n"
+        
+        exit 1
+    fi
+}
+nmap_script
+echo -e ${BLUE}"[+] Installation Done :) "
---
+++ script.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+NC='\033[0m'
+RED='\033[1;38;5;196m'
+GREEN='\033[1;38;5;040m'
+ORANGE='\033[1;38;5;202m'
+BLUE='\033[1;38;5;012m'
+BLUE2='\033[1;38;5;032m'
+PINK='\033[1;38;5;013m'
+GRAY='\033[1;38;5;004m'
+NEW='\033[1;38;5;154m'
+YELLOW='\033[1;38;5;214m'
+CG='\033[1;38;5;087m'
+CP='\033[1;38;5;221m'
+CPO='\033[1;38;5;205m'
+CN='\033[1;38;5;247m'
+CNC='\033[1;38;5;051m'
+
+function nmap_scan(){
+echo -e ${RED}    "###############################################################"
+echo -e ${ORANGE} " #     DOMAIN'S TO IP RESOLVERS & NMAP NSE SCRIPT SCAN        #  "
+echo -e ${PINK}   " #             Coded By: HAME-RU                              #  "
+echo -e ${CP}     " #             https://github.com/HAME-RU                     #  "
+echo -e ${RED}    "################################################################ \n "
+
+}
+d=$(date +"%b-%d-%y %H:%M")
+function scan_single(){
+clear
+nmap_scan
+echo -n -e ${RED}"\n[+] Enter Single domain (https://target.com) : " 
+           read domain
+mkdir -p $domain $domain/masscan $domain/nmap
+echo "$domain" > $domain/domain.txt
+echo -e ${BLUE}"\n[+] Resolving domain to IP:- \n"
+massdns -r ~/tools/resolvers/resolver.txt -t A -o S -w $domain/masscan/results.txt $domain/domain.txt
+cat $domain/masscan/results.txt | sed '/\/ /g' | awk '{print $3}' | tee $domain/masscan/ip.txt
+echo -e ${GREEN}"\n[+] NMAP NSE Scan Started On Domain:- "
+nmap -sV  --script vulners.nse -iL $domain/masscan/ip.txt  -oN $domain/nmap/scan.txt
+}
+function scan_all(){
+clear
+nmap_scan
+echo -e -n ${ORANGE}"\n[+] Enter domain name (e.g target.com) : "
+read domain
+mkdir -p $domain $domain/domain_enum $domain/final_domains  $domain/nmap  $domain/masscan 
+
+echo -e ${BLUE}"\n[+] Finding Subdomains.....:- \n"
+sleep 1
+echo -e ${CP}"\n[+] Crt.sh Started:- "
+curl -s https://crt.sh/\?q\=%25.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee $domain/domain_enum/crt.txt
+echo -e ${PINK}"\n[+] Subfinder  Started:- "
+subfinder -d $domain -o $domain/domain_enum/subfinder.txt
+
+echo -e ${YELLOW}"[+] Assetfinder Started:- "
+assetfinder -subs-only $domain | tee $domain/domain_enum/assetfinder.txt
+
+echo -e ${GREEN}"\n[+] Amass Started:- "
+amass enum -passive -d $domain -o $domain/domain_enum/amass.txt
+
+echo -e ${BLUE}"\n[+] Shuffledns Started:- "
+
+shuffledns -d $domain -w /usr/share/seclists/Discovery/DNS/deepmagic.com-prefixes-top50000.txt -r ~/tools/resolvers/resolver.txt -o $domain/domain_enum/shuffledns.txt
+echo -e ${CPO}"\n[+] Collecting All Subdomains Into Single File:- "
+cat $domain/domain_enum/*.txt > $domain/domain_enum/all.txt
+
+echo -e ${RED}"\n[+] Resolving All Subdomains:- "
+
+shuffledns -d $domain -list $domain/domain_enum/all.txt -o $domain/domains.txt -r ~/tools/resolvers/resolver.txt
+
+echo -e ${BLUE}"\n[+]Checking Services on Domains:- "
+cat $domain/domains.txt | httpx -threads 30 -o $domain/final_domains/httpx.txt
+
+echo -e ${CP}"[+] Resolving Domains to IP'S:- "
+massdns -r ~/tools/resolvers/resolver.txt -t A -o S -w $domain/masscan/results.txt $domain/domains.txt
+cat $domain/masscan/results.txt | sed '/\/ /g' | awk '{print $3}' | tee $domain/masscan/ip.txt
+
+echo -e ${GREEN}"\n[+] Started NMAP NSE Scan:- "
+
+nmap -sV  --script vulners.nse -iL $domain/masscan/ip.txt -oN $domain/nmap/scan.txt
+
+
+
+}
+function scan_list(){
+clear
+nmap_scan
+echo -n -e ${BLUE2}"\n[+] Enter path of  domains list (e.g https://target.com): "
+read host
+for domain in $(cat $host);
+do
+mkdir -p $domain $domain/masscan $domain/nmap
+echo "$domain" > $domain/domain.txt
+echo -e ${BLUE}"[+] Resolving Domains to IP:- "
+massdns -r ~/tools/resolvers/resolver.txt -t A -o S -w $domain/masscan/results.txt $domain/domain.txt
+cat $domain/masscan/results.txt | sed '/\/ /g' | awk '{print $3}' | tee $domain/masscan/ip.txt
+
+echo -e ${GREEN}"\n[+] NMAP NSE SCAN Started:- "
+
+nmap -sV  --script vulners.nse -iL $domain/masscan/ip.txt  -oN $domain/nmap/scan.txt
+
+done
+
+}
+
+menu(){
+clear
+nmap_scan
+echo -e ${YELLOW}"\n[*] Which Type of Scan u want to Perform\n "
+echo -e "  ${NC}[${CG}"1"${NC}]${CNC} Single domain Scan"
+echo -e "  ${NC}[${CG}"2"${NC}]${CNC} List of domains"
+echo -e "  ${NC}[${CG}"3"${NC}]${CNC} Full domain scan with subdomains"
+echo -e "  ${NC}[${CG}"4"${NC}]${CNC} Exit"
+
+echo -n -e ${YELLOW}"\n[+] Select: "
+        read js_play
+                if [ $js_play -eq 1 ]; then
+                        scan_single
+                elif [ $js_play -eq 2 ]; then
+                        scan_list
+                elif [ $js_play -eq 3 ]; then
+                        scan_all
+                elif [ $js_play -eq 4 ]; then
+                      exit
+                fi
+
+}
+menu
+
